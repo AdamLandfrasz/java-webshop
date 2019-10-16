6dce3ae81a7698dd7e1a40d46038c26c6b40b0cd
Bence Fabian <bence.fabian@codecool.com>
2019-10-16 15:22:04 +0200

    create fragment from template

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,7 +1,31 @@
 export function init() {
     window.addEventListener('DOMContentLoaded', (event) => {
-        alert('DOM loaded whuuuuuuu!!!');
+        const appRoot = document.getElementById('app');
+        const product = {
+            name: 'Phone',
+            description: `A simple phone.
+
+It is very cool`,
+            price: 100,
+            picUrl: '/static/img/product_1.jpg'
+        };
+        const productElement = productToElement(product);
+        appRoot.appendChild(productElement);
     });
+}
+
+function productToElement(product) {
+    const fragment = document
+          .getElementById('product-template')
+          .content
+          .cloneNode(true);
+
+    fragment.querySelector('.product-name').innerText = product.name;
+    fragment.querySelector('.product-description').innerText = product.description;
+    fragment.querySelector('.product-picture').src = product.picUrl;
+    fragment.querySelector('.product-price').innerText = product.price;
+
+    return fragment;
 }
 
 init();
