d57ec3bcec34af97a0de3c8d14292470c5f52bf1
Bence Fabian <bence.fabian@codecool.com>
2019-10-16 09:21:33 +0200

    add missing override

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -38,4 +38,9 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
     }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent contextEvent) {
+
+    }
 }
