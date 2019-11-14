d732b3a512386315db1b9eabac239317f3596c64
Adam Landfrasz <adam.landfrasz@gmail.com>
2019-11-14 21:55:15 +0100

    removed unused java file + prepped a code snippet for presentation

--- src/main/java/com/codecool/shop/controller/SetCartAmount.java
+++ src/main/java/com/codecool/shop/controller/SetCartAmount.java
@@ -31,26 +31,30 @@ public class SetCartAmount extends HttpServlet {
 
         CartDaoMem cartDataStore = CartDaoMem.getInstance();
         ProductDaoMem productDataStore = ProductDaoMem.getInstance();
+        Gson gson = new Gson();
 
         Cart sessionCart = cartDataStore.find(req.getSession().getId());
         Product product = productDataStore.find(Integer.parseInt(req.getParameter("productId")));
         Integer newAmount = Integer.parseInt(req.getParameter("newAmount"));
         sessionCart.setProductAmount(product, newAmount);
 
-        Map<Product, Integer> sessionMap = sessionCart.getCart();
+        // Accessing cart map that has Product objects as keys and Integers as values
+        Map<Product, Integer> sessionCartMap = sessionCart.getCart();
+        // Creating collection to be sent as response
         List<Map<String, String>> responseList = new ArrayList<>();
-
-        for (Product mapProduct : sessionMap.keySet()) {
+        // Converting each product in cart into a map with String keys and String values
+        // So it is possible to later convert it to a JS object and read the field
+        sessionCartMap.keySet().forEach(mapProduct -> {
             HashMap<String, String> productMap = new HashMap<>();
 
             productMap.put("id", String.valueOf(mapProduct.getId()));
             productMap.put("name", mapProduct.getName());
             productMap.put("price", String.valueOf(mapProduct.getDefaultPrice()));
-            productMap.put("amount", String.valueOf(sessionMap.get(mapProduct)));
+            productMap.put("amount", String.valueOf(sessionCartMap.get(mapProduct)));
 
             responseList.add(productMap);
-        }
-        Gson gson = new Gson();
+        });
+        // Converting response list into JSON string and sending response
         String jsonString = gson.toJson(responseList);
         resp.getWriter().println(jsonString);
     }
