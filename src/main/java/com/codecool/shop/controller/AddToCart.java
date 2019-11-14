9bb48ab990a1c6705911d77dc222e0722644983b
Adam Landfrasz <adam.landfrasz@gmail.com>
2019-11-14 22:08:42 +0100

    optimized imports

--- src/main/java/com/codecool/shop/controller/AddToCart.java
+++ src/main/java/com/codecool/shop/controller/AddToCart.java
@@ -1,34 +1,22 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.CartDaoMem;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.model.Cart;
 import com.google.gson.Gson;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import javax.servlet.http.Cookie;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet(urlPatterns = {"/add-cart"})
 public class AddToCart extends HttpServlet {
 
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         CartDaoMem cartDataStore = CartDaoMem.getInstance();
         ProductDaoMem productDataStore = ProductDaoMem.getInstance();
