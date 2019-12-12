9e4f5056e9fdd044cd6931d927926719a01b0bb8
BotondSiklosi <kissiki99@gmai.com>
2019-12-12 15:10:12 +0100

    login and register basics

---
+++ src/main/java/com/codecool/shop/controller/Register.java
@@ -0,0 +1,32 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementationWithJDBC.ProductCategoryDaoJDBC;
+import com.codecool.shop.dao.implementationWithJDBC.ProductDaoJDBC;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/register"})
+public class Register extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+
+        resp.sendRedirect("/");
+    }
+
+
+}
