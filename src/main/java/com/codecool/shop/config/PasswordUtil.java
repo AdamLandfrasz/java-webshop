e264916f9bfd538bb1c4574d637971604684bc7f
Adam Landfrasz <adam.landfrasz@gmail.com>
2019-12-12 16:40:00 +0100

    hash algorithms modified to return String input and validate String input

--- src/main/java/com/codecool/shop/config/PasswordUtil.java
+++ src/main/java/com/codecool/shop/config/PasswordUtil.java
@@ -2,6 +2,7 @@ package com.codecool.shop.config;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import java.lang.reflect.Array;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
@@ -12,6 +13,7 @@ import java.util.Random;
 public class PasswordUtil {
     private static Random random = new Random();
     private static Base64.Encoder encoder = Base64.getEncoder();
+    private static Base64.Decoder decoder = Base64.getDecoder();
 
     private static byte[] getHash(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
         KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
@@ -24,22 +26,23 @@ public class PasswordUtil {
         return saltedHash;
     }
 
-    public static byte[] hashPassword(String password) {
+    private static byte[] hashPassword(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return getHash(password, salt);
+    }
+
+    public static String hashPassword(String password) {
         byte[] salt = new byte[16];
         random.nextBytes(salt);
         try {
-            return getHash(password, salt);
+            return encoder.encodeToString(getHash(password, salt));
         } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private static byte[] hashPassword(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return getHash(password, salt);
-    }
-
-    public static boolean validatePassword(String password, byte[] hash) {
+    public static boolean validatePassword(String password, String hashedPw) {
+        byte[] hash = decoder.decode(hashedPw);
         byte[] salt = new byte[16];
         System.arraycopy(hash, 0, salt, 0, 16);
         try {
