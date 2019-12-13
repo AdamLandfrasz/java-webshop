a546256766bd6c15c4020761b2b4dea5a712432f
Adam Landfrasz <adam.landfrasz@gmail.com>
2019-12-13 09:25:57 +0100

    refactored pwutil

--- src/main/java/com/codecool/shop/config/PasswordUtil.java
+++ src/main/java/com/codecool/shop/config/PasswordUtil.java
@@ -13,27 +13,33 @@ public class PasswordUtil {
     private static Random random = new Random();
     private static Base64.Encoder encoder = Base64.getEncoder();
     private static Base64.Decoder decoder = Base64.getDecoder();
+    private static final int ITERATION_COUNT = 65536;
+    private static final int KEY_LENGTH = 128;
+    private static final int SALT_LENGTH = 16;
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA1";
 
-    private static byte[] getHash(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        byte[] hash = keyFactory.generateSecret(spec).getEncoded();
-        byte[] saltedHash = new byte[salt.length + hash.length];
+    private static byte[] getHash(char[] password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeySpec keySpec = new PBEKeySpec(password, salt, ITERATION_COUNT, KEY_LENGTH);
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(ALGORITHM);
+        byte[] hash = keyFactory.generateSecret(keySpec).getEncoded();
+        byte[] hashWithSalt = new byte[salt.length + hash.length];
 
-        System.arraycopy(salt, 0, saltedHash, 0, salt.length);
-        System.arraycopy(hash, 0, saltedHash, salt.length, hash.length);
-        return saltedHash;
+        System.arraycopy(salt, 0, hashWithSalt, 0, salt.length);
+        System.arraycopy(hash, 0, hashWithSalt, salt.length, hash.length);
+        return hashWithSalt;
     }
 
     private static byte[] hashPassword(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        return getHash(password, salt);
+        char[] passwordChars = password.toCharArray();
+        return getHash(passwordChars, salt);
     }
 
     public static String hashPassword(String password) {
-        byte[] salt = new byte[16];
+        char[] passwordChars = password.toCharArray();
+        byte[] salt = new byte[SALT_LENGTH];
         random.nextBytes(salt);
         try {
-            return encoder.encodeToString(getHash(password, salt));
+            return encoder.encodeToString(getHash(passwordChars, salt));
         } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
@@ -42,8 +48,8 @@ public class PasswordUtil {
 
     public static boolean validatePassword(String password, String hashedPw) {
         byte[] hash = decoder.decode(hashedPw);
-        byte[] salt = new byte[16];
-        System.arraycopy(hash, 0, salt, 0, 16);
+        byte[] salt = new byte[SALT_LENGTH];
+        System.arraycopy(hash, 0, salt, 0, SALT_LENGTH);
         try {
             return Arrays.equals(hashPassword(password, salt), hash);
         } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
